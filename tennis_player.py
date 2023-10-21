732318f6b0795e96815cbcf0d20b54cfd67d900c
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-21 17:22:02 +0900

    기본적인 그리기 준비 완료, 애니메이션 정보들(너비, 높이등) 찾기 시작)

--- tennis_player.py
+++ tennis_player.py
@@ -16,9 +16,21 @@ running = True
 character = load_image('tennis_character_micki.png')
 frame = 0
 
+# start_animation_y = 80 # 80부터 위로 올라가야함
+# animtion_row = 20
+
+# 애니메이션 정보 찾기
+# animation_name, (start_x, start_y, width, height, frame)
+
+frame = 0
+x_size, y_size = 30, 25
 while running:
+    clear_canvas()
+
     handle_events()
-    character.clip_draw(0, 50, 50, 50, 100, 100)
+    character.clip_draw(0, 80, 30, 25, 100, 100)
+
+    frame += 1
     update_canvas()
 
 close_canvas()
