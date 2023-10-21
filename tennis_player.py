1dc5f048bb1fe1b97a9100a240a68a36e515fa88
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-21 18:01:36 +0900

    애니메이션 별 프레임 정보를 찾기전 필요한 정보들을 주석으로 정리

--- tennis_player.py
+++ tennis_player.py
@@ -16,21 +16,34 @@ running = True
 character = load_image('tennis_character_micki.png')
 frame = 0
 
-# start_animation_y = 80 # 80부터 위로 올라가야함
-# animtion_row = 20
-
 # 애니메이션 정보 찾기
-# animation_name, (start_x, start_y, width, height, frame)
 
-frame = 0
-x_size, y_size = 30, 25
+# 현재 애니메이션은 프레임마다 정보가 다름
+# 프레임 마다 너비, 높이를 리스트에 저장
+
+# Logic
+# frame_start_x = frame_width[frame] # frame = (frame + 1) % frame_count
+# 프레임의 높이는 애니메이션 마다 다르고 같은 애니메이션 안에서는 같음
+
+# draw함수 파라미터:
+# frame_start_x, animation_start_y, frame_width[frame], frame_height, x, y
+
+# 프레임 정보를 찾는데 이용한 사이트: http://www.spritecow.com/
+
+# 애니메이션들의 시작위치는 대부분 0이나 0이 아닌 애니메이션 존재
+frame_start_x = 0
+
 while running:
     clear_canvas()
 
     handle_events()
-    character.clip_draw(0, 80, 30, 25, 100, 100)
 
-    frame += 1
+    frame = (frame + 1) % 4
+
+
+
     update_canvas()
+
+    delay(1)
 
 close_canvas()
