61d1beacc8cb0c1170ce51ac988b487e4b2e7ca2
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-21 21:08:38 +0900

    애니메이션 정보를 담은 딕셔너리 모듈화(다른 파일로 옮김)

--- tennis_player.py
+++ tennis_player.py
@@ -1,5 +1,7 @@
 from pico2d import *
 
+from animation_info import micky_animation
+
 
 def handle_events():
     global running
@@ -16,18 +18,18 @@ def handle_events():
 def draw_character_animation_in_dict(animation_name):
     global frame_start_x, frame
     if (frame == 0):
-        frame_start_x = animation[animation_name][0]
+        frame_start_x = micky_animation[animation_name][0]
 
-    width, height = animation[animation_name][2][frame], animation[animation_name][3]
+    width, height = micky_animation[animation_name][2][frame], micky_animation[animation_name][3]
     character_w, character_h = width * 3, height * 3
     # 캐릭터 크기를 키우기 위해 clip_composite_draw사용
     # 크기 비율은 3배로 늘림
-    character.clip_composite_draw(frame_start_x, animation[animation_name][1],
+    character.clip_composite_draw(frame_start_x, micky_animation[animation_name][1],
                                   width, height,
                                   0, ' ', 400, 300, character_w, character_h)
 
-    frame_start_x += animation[animation_name][2][frame]
-    frame = (frame + 1) % animation[animation_name][4]
+    frame_start_x += micky_animation[animation_name][2][frame]
+    frame = (frame + 1) % micky_animation[animation_name][4]
 
 
 open_canvas()
@@ -48,41 +50,9 @@ frame = 0
 # draw함수 파라미터:
 # frame_start_x, animation_start_y, frame_width[frame], frame_height, x, y
 
-# 프레임 정보를 찾는데 이용한 사이트: http://www.spritecow.com/
-
-# 애니메이션들의 시작위치는 대부분 0이나 0이 아닌 애니메이션 존재
-# dictionary = { animation_name: tuple(frame_start_x, frame_start_y, frame_height, farme_count)
-animation = {"Lose": (0, 78, [29, 32, 25, 32, 26], 28, 5),
-             "Win": (0, 111, [26, 35, 33, 39], 28, 4),
-             "Taunt_back": (0, 144, [18, 24, 18, 31, 26, 28], 38, 6),
-             "Taunt_front": (161, 144, [18, 24, 18, 31, 26, 28], 38, 6),
-             "Diving_left_back": (0, 252, [24, 55, 44, 38, 25], 24, 5),
-             "Diving_left_front": (223, 252, [25, 54, 48, 37, 30], 24, 5),
-             "Diving_right_back": (0, 283, [33, 57, 46, 38, 31], 31, 5),
-             "Diving_right_front": (223, 283, [36, 57, 46, 39, 32], 31, 5),
-             "Hit_left_back": (0, 318, [23, 39, 20, 28, 20], 34, 5),
-             "Hit_left_front": (223, 318, [23, 39, 20, 28, 20], 34, 5),
-             "Hit_right_back": (0, 359, [28, 42, 19, 34, 23], 33, 5),
-             "Hit_right_front": (223, 359, [27, 42, 19, 36, 24], 33, 5),
-             "Run_up": (0, 642, [25, 27, 21, 28, 21, 27], 32, 6),
-             "Run_right_up": (0, 606, [33, 38, 22, 29, 23, 34], 31, 6),
-             "Run_right": (0, 571, [29, 39, 30, 39, 33, 35], 30, 6),
-             "Run_right_down": (0, 537, [20, 27, 28, 39, 30, 26], 29, 6),
-             "Run_down": (0, 502, [26, 28, 21, 28, 21, 27], 30, 6),
-             "Run_left_down": (0, 468, [33, 38, 23, 29, 24, 34], 29, 6),
-             "Run_left": (0, 433, [29, 38, 31, 39, 35, 34], 30, 6),
-             "Run_left_up": (0, 397, [20, 26, 27, 38, 31, 25], 31, 6),
-             "Idle_back": (0, 681, [20, 25, 18, 25, 19], 28, 5),
-             "Idle_front": (157, 681, [20, 25, 18, 25, 19], 28, 5),
-             "High_hit_back": (0, 714, [25, 26, 24, 39, 25], 46, 5),
-             "High_hit_front": (157, 714, [23, 26, 25, 38, 25], 46, 5),
-             "Preparing_serve_back": (0, 765, [27, 38, 18, 31], 35, 4),
-             "Preparing_serve_front": (157, 765, [27, 37, 17, 29], 35, 4),
-             }
-
 cur_animation = "Preparing_serve_front"
 
-frame_start_x = animation[cur_animation][0]
+frame_start_x = micky_animation[cur_animation][0]
 
 # 캐릭터의 임시 위치
 test_position = (400, 300)
