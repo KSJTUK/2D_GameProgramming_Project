91fd8346e9673b4f8c0ddc1a1cd7cc7dd7b62db4
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-22 12:49:49 +0900

    테니스 캐릭터 클래스화 및 기능 모듈화

---
+++ tennis_character.py
@@ -0,0 +1,39 @@
+from animation_info import *
+from pico2d import load_image
+
+
+# 애니메이션 정보 찾기
+
+# 현재 애니메이션은 프레임마다 정보가 다름
+# 프레임 마다 너비, 높이를 리스트에 저장
+
+# Logic
+# frame_start_x = frame_width[frame] # frame = (frame + 1) % frame_count
+# 프레임의 높이는 애니메이션 마다 다르고 같은 애니메이션 안에서는 같음
+
+# draw함수 파라미터:
+# frame_start_x, animation_start_y, frame_width[frame], frame_height, x, y
+
+class Character:
+    def __init__(self):
+        self.image = load_image('tennis_character_micki.png')
+        self.x, y = 400, 300
+        self.animation = "Idle_back"
+        self.frame = 0
+        self.frame_start_x = 0
+
+    # 딕셔너리 내에 저장된 애니메이션 정보를 토대로 그려줄 함수 구현
+    def draw(self):
+        if (self.frame == 0):
+            self.frame_start_x = micky_animation[self.animation][0]
+
+        width, height = micky_animation[self.animation][2][self.frame], micky_animation[self.animation][3]
+        character_w, character_h = width * 3, height * 3
+        # 캐릭터 크기를 키우기 위해 clip_composite_draw사용
+        # 크기 비율은 3배로 늘림
+        self.image.clip_composite_draw(self.frame_start_x, micky_animation[self.animation][1],
+                                       width, height,
+                                       0, ' ', 400, 300, character_w, character_h)
+
+        self.frame_start_x += micky_animation[self.animation][2][self.frame]
+        self.frame = (self.frame + 1) % micky_animation[self.animation][4]
--- tennis_player.py
+++ tennis_player.py
@@ -1,7 +1,6 @@
 from pico2d import *
 
-from animation_info import micky_animation
-
+import tennis_character
 import tennis_court
 
 
@@ -16,46 +15,11 @@ def handle_events():
             running = False
 
 
-# 딕셔너리 내에 저장된 애니메이션 정보를 토대로 그려줄 함수 구현
-def draw_character_animation_in_dict(animation_name):
-    global frame_start_x, frame
-    if (frame == 0):
-        frame_start_x = micky_animation[animation_name][0]
-
-    width, height = micky_animation[animation_name][2][frame], micky_animation[animation_name][3]
-    character_w, character_h = width * 3, height * 3
-    # 캐릭터 크기를 키우기 위해 clip_composite_draw사용
-    # 크기 비율은 3배로 늘림
-    character.clip_composite_draw(frame_start_x, micky_animation[animation_name][1],
-                                  width, height,
-                                  0, ' ', 400, 300, character_w, character_h)
-
-    frame_start_x += micky_animation[animation_name][2][frame]
-    frame = (frame + 1) % micky_animation[animation_name][4]
-
-
 open_canvas()
 
 running = True
-character = load_image('tennis_character_micki.png')
-frame = 0
 
-# 애니메이션 정보 찾기
-
-# 현재 애니메이션은 프레임마다 정보가 다름
-# 프레임 마다 너비, 높이를 리스트에 저장
-
-# Logic
-# frame_start_x = frame_width[frame] # frame = (frame + 1) % frame_count
-# 프레임의 높이는 애니메이션 마다 다르고 같은 애니메이션 안에서는 같음
-
-# draw함수 파라미터:
-# frame_start_x, animation_start_y, frame_width[frame], frame_height, x, y
-
-cur_animation = "Preparing_serve_front"
-
-frame_start_x = micky_animation[cur_animation][0]
-
+character = tennis_character.Character()
 court = tennis_court.TennisCourt(0)
 
 # 캐릭터의 임시 위치
@@ -67,10 +31,10 @@ while running:
     handle_events()
 
     court.draw()
-    # draw_character_animation_in_dict(cur_animation)
+    character.draw()
 
     update_canvas()
 
-    # delay(0.3)
+    delay(0.3)
 
 close_canvas()
