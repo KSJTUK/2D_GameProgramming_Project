5fffb536b3fc1c343e8f637c0a649c79d77373e5
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-10-25 18:35:19 +0900

    상태기계의 틀 작성 및 Idle상태 완성

--- tennis_character.py
+++ tennis_character.py
@@ -1,15 +1,22 @@
 from animation_info import *
-from pico2d import load_image, SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT
+from pico2d import load_image
+# , SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT
 
 
+# 상태의 enter, exit 함수들은 어떤 이유에서 나가고 들어오는지를 판단하기위해
+# 이벤트를 받음
 class Idle:
     @staticmethod
-    def enter(character):
-        pass
+    def enter(character, event):
+        print("Idle Entered")
 
     @staticmethod
     def do(character):
-        pass
+        character.frame_start_x += micky_animation[character.animation][2][character.frame]
+        character.frame = (character.frame + 1) % micky_animation[character.animation][4]
+
+        if character.frame == 0:
+            character.frame_start_x = micky_animation[character.animation][0]
 
     @staticmethod
     def exit(character):
@@ -17,13 +24,37 @@ class Idle:
 
     @staticmethod
     def draw(character):
-        pass
+        # 이미지 너비, 높이 받기
+        width, height = (micky_animation[character.animation][2][character.frame],
+                         micky_animation[character.animation][3])
+        character_w, character_h = width * 3, height * 3  # 캐릭터 크기를 이미지 비율에 맞게 확대
+
+        character.image.clip_composite_draw(character.frame_start_x, micky_animation[character.animation][1],
+                                            width, height, 0, ' ',
+                                            character.x, character.y, character_w, character_h)
+
 
 # 캐릭터의 상태 기계
 class CharacterSatateMachine:
     def __init__(self, character):
         self.character = character
-        self.cur_state = None
+        self.cur_state = Idle
+        self.transition_state_dic = {
+            None
+        }
+
+    def start(self):
+        self.cur_state.enter(self.character, ('NONE', 0))
+
+    def update(self):
+        self.cur_state.do(self.character)
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transition_state_dic[self.cur_state].items():
+            pass
+
+    def draw(self):
+        self.cur_state.draw(self.character)
 
 
 # 애니메이션 정보 찾기
@@ -47,31 +78,18 @@ class Character:
         self.frame_start_x = 0
         self.dir_x, self.dir_y = 0, 0
 
+        # 캐릭터의 상태기계 생성
+        self.state_machine = CharacterSatateMachine(self)
+        self.state_machine.start()
+
     # 캐릭터 애니메이션 프레임 업데이트
     def update(self):
-        self.x += self.dir_x
-
-        # 프레임 업데이트 코드
-        self.frame_start_x += micky_animation[self.animation][2][self.frame]
-        self.frame = (self.frame + 1) % micky_animation[self.animation][4]
-
-        if (self.frame == 0):
-            self.frame_start_x = micky_animation[self.animation][0]
+        self.state_machine.update()
 
     def handle_event(self, event):
-        if event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                self.dir_x = 1
-            elif event.key == SDLK_LEFT:
-                self.dir_x = -1
+        # self.state_machine.handle_event('INPUT', event)
+        pass
 
     # 딕셔너리 내에 저장된 애니메이션 정보를 토대로 그려줄 함수 구현
     def draw(self):
-        width, height = micky_animation[self.animation][2][self.frame], micky_animation[self.animation][3]
-        character_w, character_h = width * 3, height * 3
-
-        # 캐릭터 크기를 키우기 위해 clip_composite_draw사용
-        # 크기 비율은 3배로 늘림
-        self.image.clip_composite_draw(self.frame_start_x, micky_animation[self.animation][1],
-                                       width, height,
-                                       0, ' ', self.x, self.y, character_w, character_h)
+        self.state_machine.draw()
