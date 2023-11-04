2d74b8572118ef626ae8d15a3cd5b0b081fadbb2
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-11-04 18:37:45 +0900

    기존 게임루프가 있던 파일 이름 main으로 수정, 수업시간에 배운 게임 프레임 워크 적용

--- play_mode.py
+++ play_mode.py
@@ -1,6 +1,6 @@
 from pico2d import *
 
-# import game_framework
+import game_framework
 
 import game_world
 from tennis_court import TennisCourt
@@ -10,10 +10,10 @@ def handle_events():
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
-            # game_framework.quit()
+            game_framework.quit()
             pass
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            # game_framework.quit()
+            game_framework.quit()
             pass
         else:
             character.handle_event(event)
