467223fead14c816685373839475d72d315ad625
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-11-26 23:44:05 +0900

    score mode에서 키입력을 받아 종료할 수 있도록 함

--- codes/score_mode.py
+++ codes/score_mode.py
@@ -1,8 +1,9 @@
 import tennis_game_score
 import tennis_referee
+import game_framework
 import game_world
 
-from pico2d import clear_canvas, update_canvas
+from pico2d import clear_canvas, update_canvas, get_events, SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
 
 
 def init():
@@ -27,4 +28,9 @@ def render():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
